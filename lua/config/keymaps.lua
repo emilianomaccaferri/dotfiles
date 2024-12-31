edc84705ab4282fbc48f47a9a0e27a6904ee8605
Emiliano Maccaferri <donglerio@unimore.it>
2024-12-31 13:53:52 +0100

    add: os detection for clipboard

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,3 +1,6 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
--- Add any additional keymaps here
+-- Add any additional keymaps her
+--
+local map = LazyVim.safe_keymap_set
+map("n", "<C-a>", "<cmd>bn<cr>", { desc = "switch tabbuf", expr = true, silent = true })
