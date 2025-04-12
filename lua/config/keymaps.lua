19bd3c40dec2801afc871efecb234dca6cea1b73
Emiliano Maccaferri <emiliano@zuru.com>
2025-04-12 12:23:35 +0200

    add: keybindings and markdown preview

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -2,3 +2,4 @@
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 local map = LazyVim.safe_keymap_set
 map("n", "<C-t>", "<cmd>Neotree toggle<cr>", { desc = "toggle neotree" })
+map("n", "<Leader>fl", "<cmd>FzfLua live_grep<cr>", { desc = "toggle live_grep" })
